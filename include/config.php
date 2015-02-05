d180533a1f484652eae0771cc1b9a1f4d78f4a04
Mihai <mihai@invata-programare.ro>
2015-02-05 17:25:25 +0200

    Database Connection

--- include/config.php
+++ include/config.php
@@ -1,9 +1,19 @@
 <?php
 
 //Database Credentials
-
+	$db_driver	= '';
+	$db_host 	= '';
+	$db_user 	= '';
+	$db_pass 	= '';
+	$db_db 		= '';
 //Database Connection
-
+try{
+	$db = new PDO("{$db_driver}={$db_host};dbname={$db_db};", $db_user, $db_pass);
+}catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
 //script variables
+
 
 //autoloader
